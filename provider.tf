9674436f5e7fef1658b6925d158923f85a4b78e6
Douglas Lima <32877533+dougls@users.noreply.github.com>
2024-03-04 20:23:19 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "<colocar-nome-bucket>"
+    bucket = "<colocar-nome-bucket>" # example: terraform-tfstates
     key    = "sonarqube/terraform.tfstate"
-    region = "<colocar-regiao>"
+    region = "<colocar-regiao>"  # example: us-east-1 (região escolhida na criação do bucket)
   }
 }
 
 provider "aws" {
-    profile = "default"
+    profile = "default" # trocar pelo profile que tiver, ou retirar se utilizar o aws configure sem um profile
   region = var.regionDefault
 
   default_tags {
